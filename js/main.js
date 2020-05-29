4a8654b778bc50dffa0e8036acb5395a473d2eb1
jszelag777 <joannaszelag777@gmail.com>
2020-05-29 11:01:00 +0200

    Rozwiązanie zadania

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = 5;
+secondNumber = 10;
+
+function addition(firstNumber, secondNumber){
+    let result = firstNumber + secondNumber;
+    return result;
+}
